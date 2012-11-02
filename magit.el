5135e5a423b93e11d4f8440a7d51cca3d9ad3652
Michal Maruska <mmaruska@gmail.com>
2012-11-02 12:27:16 +0100

    The other HALF: possibly insert .git/MERGE_MSG, but in certain Git repositories prepend the [branch] to the commit message.

--- magit.el
+++ magit.el
@@ -3576,7 +3576,31 @@ continue it.
 			    "Nothing staged.  Commit all unstaged changes? "))
 		       "yes" "no")))))
 	 (when amend-p (magit-log-edit-toggle-amending))
-	 (magit-pop-to-log-edit "commit"))))
+	 ;; if under the estrella..
+
+
+	 (with-current-buffer (magit-get-log-edit-buffer)
+	   ;; So, this should be SHOWN to the user. It does contain
+	   ;; stuff, that magit cannot know!
+	   (when (file-exists-p ".git/MERGE_MSG")
+	     (erase-buffer)
+	     (insert-file-contents ".git/MERGE_MSG"))
+
+	   (if (string-match "/next/" default-directory)
+	       (unless ;; already there ...
+		   (save-excursion
+		     (goto-char (point-min))
+		     (or
+		      (looking-at "\\[")
+		      (and
+		       (search-forward "-- End of Magit header --" nil t)
+		       (next-line)
+		       (looking-at "\\["))))
+		 (magit-log-edit-append
+		  ;;  fixme: this goes AFTER. Also, if I invoke this twice ....
+		  (concat "[" (magit-get-current-branch) "] ") t)))
+
+	 (magit-pop-to-log-edit-half "commit")))))
 
 (defun magit-add-log ()
   (interactive)
